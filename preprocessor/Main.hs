4a097de710b72d5c33842ca5e30de33323802829
Justus Adam <dev@justus.science>
2016-11-04 15:45:39 +0100

    Preliminary external scripts support

--- preprocessor/Main.hs
+++ preprocessor/Main.hs
@@ -6,13 +6,15 @@ import           System.FilePath
 import           Text.Mustache
 import           Text.Mustache.Compile
 import           Options.Applicative
+import Data.Aeson hiding (object)
 
 
 data Opts = Opts 
     { adapter :: String
     , sourceName :: FilePath
     , sourceLocation :: FilePath
-    , targetFile :: FilePath 
+    , targetFile :: FilePath
+    , externalScripts :: FilePath
     }
 
 
@@ -34,10 +36,18 @@ main = do
     let dir = takeDirectory sourceName
     paths <- filter (not . ((||) <$> isPrefixOf "_" <*> isPrefixOf ".")) <$> getDirectoryContents dir
     files <- filterM (doesFileExist . (dir </>)) paths
+    externals <- do
+        exists <- doesFileExist externalScripts
+        if exists
+            then do
+                f <- readFile externalScripts
+                either error return $ eitherDecode f
+            else return mempty
     let hsFiles = map dropExtensions $ filter (/= takeFileName sourceName) $ filter ((`elem` [".hs", ".lhs"]) . takeExtension) files
+        scripts = hsFiles ++ externals
         (adapterImport, adapterType) = fromMaybe slackRtmData $ lookup adapter adapters
-        processed = substitute tpl (object [ "scripts" ~> intercalate ", " (map (++ ".script") hsFiles)
-                                            , "imports" ~> hsFiles
+        processed = substitute tpl (object [ "scripts" ~> intercalate ", " (map (++ ".script") scripts)
+                                            , "imports" ~> scripts
                                             , "adapter-import" ~> adapterImport
                                             , "adapter-type" ~> adapterType 
                                             ])
@@ -58,3 +68,11 @@ main = do
         <*> argument str (metavar "NAME")
         <*> argument str (metavar "PATH")
         <*> argument str (metavar "PATH")
+        <*> strOption 
+            (  long "external-scripts"
+            ++ short 's'
+            ++ value "external-scripts.json"
+            ++ metavar "PATH"
+            ++ help "config file of external scripts to load"
+            ++ showDefault
+            )
