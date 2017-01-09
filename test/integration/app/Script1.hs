2ee221da9a2ce4cab34206530d505138c4cb8a21
Justus Adam <dev@justus.science>
2017-01-09 16:03:49 +0100

    Fixing script

--- test/integration/app/Script1.hs
+++ test/integration/app/Script1.hs
@@ -14,7 +14,7 @@ script :: IsAdapter a => ScriptInit a
 script = defineScript "test" $ do
     hear (r [CaseInsensitive] "^ping$") $ do
         msg <- getMessage
-        logInfoN $(isT "#{content msg}")
+        logInfoN $(isT "#{msg}")
         send "Pong"
     respond "hello" $
         reply "Hello to you too"
