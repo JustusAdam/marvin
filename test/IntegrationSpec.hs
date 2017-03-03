a2f01f156922d56a70359ccbef7418c0ac7c9184
Justus Adam <dev@justus.science>
2017-03-03 16:09:39 +0100

    Fixed setup for travis
    Added dedicated stack.yaml files for different resolvers
    Passing resolvers as env args for integration tests
    Tests now read the configures stack.yaml, modify it and then use it

--- test/IntegrationSpec.hs
+++ test/IntegrationSpec.hs
@@ -5,6 +5,12 @@ import           System.Info.Extra
 import           System.IO.Temp
 import           System.Process
 import           Test.Hspec
+import Data.Yaml
+import Data.Aeson.Lens
+import Control.Lens
+import System.Environment
+import Data.Maybe
+import Data.Text (Text)
 
 stackBuildArgs :: [String]
 stackBuildArgs
@@ -20,22 +26,60 @@ copyDirectory source target = do
     mapM_ (copyFile <$> (source </>) <*> (target </>)) files
     mapM_ (copyDirectory <$> (source </>) <*> (target </>)) dirs
 
+
+-- | In the .travis.yml file I pass the stack yaml as STACK_YAML env variable.
+-- This is necessary as the extra-deps are different for some lts versions
+-- However the yaml file in STACK_YAML cannot be used by the integration tests, 
+-- as they require both marvins source directory as well as their own to be 
+-- recorded in the `packages` key. 
+-- This function reads the stack.yaml, modifies the `packages` section and 
+-- writes a new stack.yaml to the target path which the tests pass to stack via 
+-- then `--stack-yaml` command line parameter
+getAndModifyStackYaml :: FilePath -> IO ()
+getAndModifyStackYaml targetPath = do
+    path <- fromMaybe "test/resources/stack.yaml" <$> lookupEnv "STACK_YAML"
+    Just yamlFile <- decodeFile path
+    encodeFile targetPath $ (yamlFile :: Value) & key "packages" .~ packageObject
+  where
+    packageObject = toJSON [".", ".." :: Text]
+
+
+getResolver :: IO String
+getResolver = fromMaybe "lts" <$> lookupEnv "STACK_RESOLVER"
+
+
 -- | This test is used to make sure the initializer produces a compileable project
 runInitAndCompileResult :: IO ()
 runInitAndCompileResult =
-    withTempDirectory "." ".test" $ \dir ->
+    withTempDirectory "." ".test" $ \dir -> do
+        getAndModifyStackYaml (dir </> "stack.yaml")
         withCurrentDirectory dir $ do
-            callProcess "stack" ["exec", "--", "marvin-init", "-a", "shell", "testbot"]
-            copyFile "../test/resources/stack.yaml" "stack.yaml"
-            callProcess "stack" $ "build" : stackBuildArgs
+            resolver <- getResolver
+            callProcess "stack" 
+                [ "exec"
+                , "--resolver", resolver
+                , "--stack-yaml", "stack.yaml"
+                , "--", "marvin-init"
+                    , "-a", "shell"
+                    , "testbot"]
+            callProcess "stack" $ 
+                [ "build"
+                , "--stack-yaml", "stack.yaml"
+                , "--resolver", resolver
+                ] ++ stackBuildArgs
 
 
 compileIntegration :: IO ()
 compileIntegration =
     withTempDirectory "." ".test" $ \dir -> do
+        resolver <- getResolver
         copyDirectory "test/integration" dir
-        copyFile "test/resources/stack.yaml" (dir </> "stack.yaml")
-        withCurrentDirectory dir $ callProcess "stack" $ "build" : stackBuildArgs
+        getAndModifyStackYaml (dir </> "stack.yaml")
+        withCurrentDirectory dir $ callProcess "stack" $ 
+            [ "build"
+            , "--stack-yaml", "stack.yaml"
+            , "--resolver", resolver
+            ] ++ stackBuildArgs
 
 
 main :: IO ()
