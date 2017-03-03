54bd91415e1b20bba90d2fb9c621e104b912f2d5
Justus Adam <dev@justus.science>
2017-03-03 22:39:08 +0100

    Stylish Haskell

--- test/IntegrationSpec.hs
+++ test/IntegrationSpec.hs
@@ -1,16 +1,16 @@
+import           Control.Lens
 import           Control.Monad.Extra
+import           Data.Aeson.Lens
+import           Data.Maybe
+import           Data.Text           (Text)
+import           Data.Yaml
 import           System.Directory
+import           System.Environment
 import           System.FilePath
 import           System.Info.Extra
 import           System.IO.Temp
 import           System.Process
 import           Test.Hspec
-import Data.Yaml
-import Data.Aeson.Lens
-import Control.Lens
-import System.Environment
-import Data.Maybe
-import Data.Text (Text)
 
 stackBuildArgs :: [String]
 stackBuildArgs
@@ -29,11 +29,11 @@ copyDirectory source target = do
 
 -- | In the .travis.yml file I pass the stack yaml as STACK_YAML env variable.
 -- This is necessary as the extra-deps are different for some lts versions
--- However the yaml file in STACK_YAML cannot be used by the integration tests, 
--- as they require both marvins source directory as well as their own to be 
--- recorded in the `packages` key. 
--- This function reads the stack.yaml, modifies the `packages` section and 
--- writes a new stack.yaml to the target path which the tests pass to stack via 
+-- However the yaml file in STACK_YAML cannot be used by the integration tests,
+-- as they require both marvins source directory as well as their own to be
+-- recorded in the `packages` key.
+-- This function reads the stack.yaml, modifies the `packages` section and
+-- writes a new stack.yaml to the target path which the tests pass to stack via
 -- then `--stack-yaml` command line parameter
 getAndModifyStackYaml :: FilePath -> IO ()
 getAndModifyStackYaml targetPath = do
@@ -55,14 +55,14 @@ runInitAndCompileResult =
         getAndModifyStackYaml (dir </> "stack.yaml")
         withCurrentDirectory dir $ do
             resolver <- getResolver
-            callProcess "stack" 
+            callProcess "stack"
                 [ "exec"
                 , "--resolver", resolver
                 , "--stack-yaml", "stack.yaml"
                 , "--", "marvin-init"
                     , "-a", "shell"
                     , "testbot"]
-            callProcess "stack" $ 
+            callProcess "stack" $
                 [ "build"
                 , "--stack-yaml", "stack.yaml"
                 , "--resolver", resolver
@@ -75,7 +75,7 @@ compileIntegration =
         resolver <- getResolver
         copyDirectory "test/integration" dir
         getAndModifyStackYaml (dir </> "stack.yaml")
-        withCurrentDirectory dir $ callProcess "stack" $ 
+        withCurrentDirectory dir $ callProcess "stack" $
             [ "build"
             , "--stack-yaml", "stack.yaml"
             , "--resolver", resolver
